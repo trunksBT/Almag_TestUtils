1892a968aa5d8ed0197e2bc2af36b020e23e8835
pkorycin <pkorycin>
2020-01-07 19:47:45 +0100

    Extract communicators to externalLibraries

--- HDLC/DataLinkLayerCommunicators/ZeroMqUtils.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <zmq.hpp>
-#include <string>
-
-std::string s_recv (zmq::socket_t & socket);
-bool s_send (zmq::socket_t & socket, const std::string & string,
-             zmq::send_flags flags=zmq::send_flags::none);
