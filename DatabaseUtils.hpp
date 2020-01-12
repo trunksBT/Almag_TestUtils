ed83c61a754ebb99fd3fca2929ee589facbc0c00
pkorycin <pkorycin>
2020-01-12 21:46:59 +0100

    Remove redundant file

--- DatabaseUtils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-const std::string IOPATHS_1_UK = "/IOPaths_1";
-const std::string IOPATHS_2_UK = "/IOPaths_2";
-const std::string IOPATHS_3_UK = "/IOPaths_3";
-const std::string IOPATHS_5_UK = "/IOPaths_5";
-const std::string IOPATHS_OT = "IOPaths";
--- Hardcodes.hpp
+++ Hardcodes.hpp
@@ -20,6 +20,12 @@ const std::string BUFFER_TO_SEND_VAL_3 = TEST_BUFFERS_PATH.string() + "/ToSend3.
 
 const std::string ADDRESS_OF_PORT_FOR_ZERO_MQ = "/home/pkorycin/ZeroMqBuffer"s;
 
+const std::string IOPATHS_1_UK = "/IOPaths_1";
+const std::string IOPATHS_2_UK = "/IOPaths_2";
+const std::string IOPATHS_3_UK = "/IOPaths_3";
+const std::string IOPATHS_5_UK = "/IOPaths_5";
+const std::string IOPATHS_OT = "IOPaths";
+
 }  // namespace IOPaths
 }  // namespace hardcodes
 
